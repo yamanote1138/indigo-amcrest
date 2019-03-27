0491c7f5eb08543b012d1d81c169ce289b57a276
Chad Francis <yamanote1138@gmail.com>
2019-03-27 01:44:56 -0600

    initial commit

---
+++ Contents/Server Plugin/plugin.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python
+
+import httplib, urllib2, smtplib, sys, os
+try:
+	import xml.etree.cElementTree as ET
+except ImportError:
+	import xml.etree.ElementTree as ET
+
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+class Plugin(indigo.PluginBase):
+
+	def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
+		indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
+		self.debug = True
+
+	def __del__(self):
+		indigo.PluginBase.__del__(self)
+
+	def startup(self):
+		self.debugLog(u"startup called")
+
+	def shutdown(self):
+		self.debugLog(u"shutdown called")
+
+	def xmitToCamera(self, cgiPath, params, dev):
+
+		if cgiPath is None: 
+			return self.debugLog("no path defined")
+
+		url = 'http://%s/cgi-bin/%s.cgi?' % (dev.pluginProps['ipaddress'], cgiPath)
+
+		for param in params:
+			url = url + '&%s=%s' % (param, params[param])
+
+		# url = 'http://%s/%s?user=%s&pwd=%s' % (dev.pluginProps['ipaddress'], cgiPath, dev.pluginProps['username'], dev.pluginProps['password'])
+
+
+		self.debugLog(u"url xmitted: "+url)
+
+		return urllib2.urlopen(url)
+
+	# move camera to preset position
+	def goToPreset(self, pluginAction, dev):
+
+		self.debugLog(u"goToPreset called")
+		if dev is None: 
+			return self.debugLog(u"no device defined")
+
+		preset = pluginAction.props['preset']
+		if preset is None: 
+			return self.debugLog(u"no preset defined")
+
+		self.xmitToCamera("decoder_control.cgi", {'command': str(self.presets[preset]['go'])}, dev)
+
+	#Enable Infrared LEDs
+	def irOn(self, pluginAction, dev):
+	
+		self.debugLog(u"irOn called")
+		if dev is None: 
+			return self.debugLog(u"no device defined")
+		self.xmitToCamera("decoder_control.cgi", {'command':95}, dev)
+
+	#Disable Infrared LEDs
+	def irOff(self, pluginAction, dev):
+	
+		self.debugLog(u"irOff called")
+		if dev is None: 
+			return self.debugLog(u"no device defined")
+		self.xmitToCamera("decoder_control.cgi", {'command':94}, dev)
+
+	def snap(self, pluginAction, dev):
+	
+		self.debugLog(u"snap called")
+		if dev is None: 
+			return self.debugLog(u"no device defined")
+		resp = self.xmitToCamera('snapshot.cgi', {}, dev)
+		snapimg = resp.read()
+
+		# todo: serialize filename, pass to sendViaEmail
+		snappath = "/tmp/snap.jpg"
+		f = open(snappath, 'w')
+		f.write(snapimg)
+		f.close()
+
+		self.sendViaEmail(pluginAction, dev)
+
+	def sendViaEmail(self, pluginAction, dev):
+		
+		# Create the container (outer) email message.
+		msg = MIMEMultipart()
+		msg['Subject'] = pluginAction.props['subject']
+		msg['From'] = self.pluginPrefs['sender']
+		msg['To'] = pluginAction.props['recipient']
+		msg.preamble = 'cam snap test'
+
+		# Open the files in binary mode.  Let the MIMEImage class automatically
+		# guess the specific image type.
+		fp = open('/tmp/snap.jpg', 'rb')
+		img = MIMEImage(fp.read(), "jpeg")
+		fp.close()
+		msg.attach(img)
+
+		s = smtplib.SMTP(self.pluginPrefs['smtphost'],int(self.pluginPrefs['smtpport']))
+		s.ehlo()
+		s.starttls()
+		s.ehlo
+		s.login(self.pluginPrefs['smtpuser'], self.pluginPrefs['smtppass'])
+		s.sendmail(self.pluginPrefs['sender'], pluginAction.props['recipient'], msg.as_string())
+		s.quit()
---
+++ README.md
@@ -0,0 +1,32 @@
+indigo-foscam
+=============
+
+Indigo plugin to control Foscam webcams.
+
+### Installation Instructions
+
+1. Download latest release [here](https://github.com/discgolfer1138/indigo-foscam/releases)
+2. Follow [standard plugin installation process](http://bit.ly/1e1Vc7b)
+
+### Actions Supported
+
+- Move Camera (up, down, left, right)
+- Go to or set preset position
+- Stop Movement
+- Enable/Disable Streams
+- Activate/Deactivate Motion Alarm
+- Enable/Disable IR leds
+- Email Snapshot (SMTP server is set in plugin config)
+
+### States Exposed
+
+- X Position
+- Y Position
+
+### Supported Cameras
+
+Due to differences in the cgi paths, this plugin does not yet support the 9000-series Foscam HD cameras at this time. (models FI9805E, FI9805W, FI9821W)
+
+### Thanks
+
+Huge thanks to [Tim Hamer](https://github.com/tim-hamer) for his huge contributions to the newly-expanded list of supported actions
