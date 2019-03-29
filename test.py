22a94b503eb66116695d90180c19db975d61debf
Chad Francis <yamanote1138@gmail.com>
2019-03-28 22:55:50 -0600

    fixed issue with ordered paramters

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ from requests.auth import HTTPDigestAuth
 
 def xmitToCamera(cgiPath, params):
 
+  # note: params has to be a sequence of two-valud tuples in order to stay in order
+  # unfortunately, Amcrest's API doesn't work if the params aren't in a set order
+
   host = '192.168.1.13'
   username = 'admin'
   password = '1ts4tr4p!'
@@ -13,15 +16,8 @@ def xmitToCamera(cgiPath, params):
   url = 'http://%s/cgi-bin/%s.cgi' % (host, cgiPath)
   r = requests.get(url, params=params, auth=HTTPDigestAuth(username, password))
 
-  # url = 'http://192.168.1.13/cgi-bin/ptz.cgi?action=start&channel=0&code=GotoPreset&arg1=0&arg2=1&arg3=0&arg4=0'
-  # r = requests.get(url, auth=HTTPDigestAuth(username, password))
-
   print(u"url: "+r.url)
   print(u"res: "+r.text)
-
-  # http://192.168.1.13/cgi-bin/ptz.cgi?action=start&channel=0&code=GotoPreset&arg1=0&arg2=1&arg3=0&arg4=0'
-  # http://192.168.1.13/cgi-bin/ptz.cgi?action=start&channel=0&code=GotoPreset&arg1=0&arg2=1&arg3=0&arg4=0'
-  # http://192.168.1.13/cgi-bin/ptz.cgi?code=GotoPreset&arg1=0&arg2=1&arg3=0&arg4=0&action=start&channel=1
 
 # move camera to preset position
 def goToPreset():
