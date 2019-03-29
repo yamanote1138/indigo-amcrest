51b6f27dff46ef9b1623b4c3e84324b61c67377e
Chad Francis <yamanote1138@gmail.com>
2019-03-28 22:53:23 -0600

    fixed issue with ordered paramters

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import requests
+import json
 from requests.auth import HTTPDigestAuth
 
 def xmitToCamera(cgiPath, params):
@@ -9,19 +10,31 @@ def xmitToCamera(cgiPath, params):
   username = 'admin'
   password = '1ts4tr4p!'
 
-  # url = 'http://%s/cgi-bin/%s.cgi?' % (host, cgiPath)
-  # for param in params:
-  #   url = url + '&%s=%s' % (param, params[param])
+  url = 'http://%s/cgi-bin/%s.cgi' % (host, cgiPath)
+  r = requests.get(url, params=params, auth=HTTPDigestAuth(username, password))
 
-  url = 'http://192.168.1.13/cgi-bin/ptz.cgi?action=start&channel=0&code=GotoPreset&arg1=0&arg2=1&arg3=0&arg4=0'
+  # url = 'http://192.168.1.13/cgi-bin/ptz.cgi?action=start&channel=0&code=GotoPreset&arg1=0&arg2=1&arg3=0&arg4=0'
+  # r = requests.get(url, auth=HTTPDigestAuth(username, password))
 
-  print(u"url xmitted: "+url)
-  requests.get(url, auth=HTTPDigestAuth(username, password))
+  print(u"url: "+r.url)
+  print(u"res: "+r.text)
+
+  # http://192.168.1.13/cgi-bin/ptz.cgi?action=start&channel=0&code=GotoPreset&arg1=0&arg2=1&arg3=0&arg4=0'
+  # http://192.168.1.13/cgi-bin/ptz.cgi?action=start&channel=0&code=GotoPreset&arg1=0&arg2=1&arg3=0&arg4=0'
+  # http://192.168.1.13/cgi-bin/ptz.cgi?code=GotoPreset&arg1=0&arg2=1&arg3=0&arg4=0&action=start&channel=1
 
 # move camera to preset position
 def goToPreset():
   print(u"goToPreset called")
-  preset = '1'
-  xmitToCamera("ptz", {'action':'start','channel':'0','code':'GotoPreset','arg1':'0','arg2':'1','arg3':'0','arg4':'0'})
+  params = (
+    ('action','start'),
+    ('channel',0),
+    ('code','GotoPreset'),
+    ('arg1',0),
+    ('arg2',1),
+    ('arg3',0),
+    ('arg4',0)
+  )
+  xmitToCamera("ptz", params)
 
 goToPreset()
